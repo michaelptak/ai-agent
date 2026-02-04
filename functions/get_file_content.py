2e99f43cf220dc306e5ce0e8481e47bebec046bb
Michael Ptak <michael.p11@proton.me>
2026-02-04 08:01:07 -0500

    up to lesson 10

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,14 +1,20 @@
 import os
 
+MAX_CHARS = 10000
+
 def get_file_content(working_directory, file_path):
     try:
-        abs_path = os.path.abspath(working_directory)
-        target_file = os.path.normpath(os.path.join(abs_path, file_path))
-        if os.path.commonpath([abs_path, target_file]) != abs_path:
+        abs_dir = os.path.abspath(working_directory)
+        abs_file_path = os.path.normpath(os.path.join(abs_dir, file_path))
+        if os.path.commonpath([abs_dir, abs_file_path]) != abs_dir:
             return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-        if not os.path.isfile(target_file):
+        if not os.path.isfile(abs_file_path):
             return f'Error: File not found or is not a regular file: "{file_path}"'
-
+        with open(abs_file_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            if f.read(1):
+                file_content_string += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+        return file_content_string
 
     except Exception as e:
         return f"Error: {e}"
